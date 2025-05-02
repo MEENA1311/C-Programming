3ef93ece590f2745033f5fb27e08118320039d67
MEENA1311 <meena197110@gmail.com>
2025-05-02 20:17:49 +0530

    Create Exp 20.c

---
+++ Exp 20.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+int main() {
+    int n, arr[100];
+    scanf("%d", &n);
+    for (int i = 0; i < n; i++) scanf("%d", &arr[i]);
+    int max = arr[0];
+    for (int i = 1; i < n; i++)
+        if (arr[i] > max) max = arr[i];
+    printf("Max = %d\n", max);
+    return 0;
+}
